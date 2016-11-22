e549bc50df32b7eca2ee90bf82f3f4e012aea3e1
Unknown <t4trjo00@students.oamk.fi>
2016-11-22 09:39:42 +0200

    bootstrap css-framework lisätty

--- application/views/menu/header.php
+++ application/views/menu/header.php
@@ -2,23 +2,10 @@
 <html>
 <head>
 <title>asiakas sivusto</title>
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('style/css/bootstrap.min.css');?>">
 </head>
 
-<style type="text/css">
-/*****************************************/
-.intro{
-  margin-top:10px;
-  margin-bottom:10px;
-  padding:3px;
-  font-size:11px;
-  font-family:Verdana;
-  background-color: #E7E7FD;
-  border-top:1px solid #4f4f97;
-  border-bottom:1px solid #4f4f97;
-}
-/*****************************************/
-</style>
+
 
 
 
